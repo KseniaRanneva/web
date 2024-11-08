1a735793a316cbc05319ff56da4d4b496a22540f
KseniaRanneva <ksenia.ranneva.2000@gmail.com>
2024-11-08 19:39:56 +0300

    Adding ESLint and Prettier

--- armageddon-app/src/components/asteroidCard/AsteroidCard.tsx
+++ armageddon-app/src/components/asteroidCard/AsteroidCard.tsx
@@ -1,7 +1,7 @@
-import {AsteroidCardImage} from "./asteroidCardImage/AsteroidCardImage";
-import {AsteroidCardContent} from "./asteroidCardContent/AsteroidCardContent";
-import {AsteroidCardAction} from "./asteroidCardAction/AsteroidCardAction";
-import styles from "./AsteroidCard.module.css";
+import { AsteroidCardImage } from './asteroidCardImage/AsteroidCardImage';
+import { AsteroidCardContent } from './asteroidCardContent/AsteroidCardContent';
+import { AsteroidCardAction } from './asteroidCardAction/AsteroidCardAction';
+import styles from './AsteroidCard.module.css';
 
 //Определение типа для props
 type AsteroidCardProps = {
@@ -10,20 +10,29 @@ type AsteroidCardProps = {
     date: string;
     distance: {
         kilometers: number;
-        lunar: number
+        lunar: number;
     };
     size: number;
     isDangerous: boolean;
-    distanceMode: boolean
-}
+    distanceMode: boolean;
+};
 
 export const AsteroidCard = (props: AsteroidCardProps) => {
-    const {id, name, date, distance, size, isDangerous, distanceMode} = props;
-    return <div className={styles.card}>
-        <div className={isDangerous ? styles.redCard : styles.regularCard}>
-            <AsteroidCardImage/>
-            <AsteroidCardContent id={id} name={name} date={date} distance={distance} size={size} distanceMode={distanceMode}/>
-            <AsteroidCardAction isDangerous={isDangerous}/>
+    const { id, name, date, distance, size, isDangerous, distanceMode } = props;
+    return (
+        <div className={styles.card}>
+            <div className={isDangerous ? styles.redCard : styles.regularCard}>
+                <AsteroidCardImage />
+                <AsteroidCardContent
+                    id={id}
+                    name={name}
+                    date={date}
+                    distance={distance}
+                    size={size}
+                    distanceMode={distanceMode}
+                />
+                <AsteroidCardAction isDangerous={isDangerous} />
+            </div>
         </div>
-    </div>
-}
+    );
+};
